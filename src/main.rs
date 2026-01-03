d5efe915c17e5d279cfb12c364f60c51200ebeb6
aidankeighron <aidankeighron@gmail.com>
2026-01-02 22:57:10 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use kalosm::sound::*;
 
+//$env:RUSTFLAGS="-L native=C:\Progra~1\NVIDIA~1\CUDA\v13.1\lib\x64"
+//$env:NVCC_APPEND_FLAGS="-D_ALLOW_COMPILER_AND_STL_VERSION_MISMATCH"
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     println!("New Whisper...");
