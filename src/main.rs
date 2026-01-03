6ac0b7b3770b7c8148083c4d0da7cca561ae293a
aidankeighron <aidankeighron@gmail.com>
2026-01-02 23:15:01 -0600

    WIP transcribe

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,12 @@ async fn main() -> Result<(), anyhow::Error> {
     println!("Transcribing stream...");
     let mut text_stream = stream.transcribe(model);
 
-    while let Some(segment) = text_stream.next().await {
-        print!("{}", segment.text());
-        use std::io::{self, Write};
-        io::stdout().flush()?;
-    }
+    text_stream.to_std_out().await?;
+    // while let Some(segment) = text_stream.next().await {
+    //     print!("\r{}", segment.text()); 
+    //     use std::io::{self, Write};
+    //     io::stdout().flush()?;
+    // }
 
     Ok(())
 }
