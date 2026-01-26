51d06249a24e61cedd4ae8f4709fe56924995210
aidankeighron <aidankeighron@gmail.com>
2026-01-25 21:33:47 -0500

    update logging

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -7,10 +7,11 @@ tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 async def execute_web_search(params: FunctionCallParams):
     query = params.arguments.get("query")
-    logging.info(f"Searching: {query}")
 
+    logging.info(f"Searching: {query}")
     response = tavily.search(query=query, search_depth="basic", max_results=1)
     context = "\n".join([r["content"] for r in response["results"]])
+    logging.info(f"Got results: {context}")
     
     await params.result_callback({"result": context})
 
@@ -31,9 +32,10 @@ async def monitor_resources(params: FunctionCallParams):
     memory_info = process.memory_info()
     ram_used_mb = memory_info.rss / (1024 * 1024)
 
+    logging.info(f"Requesting usage")
     process.cpu_percent(interval=0.1) 
     cpu_usage_percent = process.cpu_percent(interval=None)
-    logging.info(f"Requesting usage: CPU {cpu_usage_percent} RAM {ram_used_mb}")
+    logging.info(f"Usage: CPU {cpu_usage_percent} RAM {ram_used_mb}")
     
     await params.result_callback({"ram": ram_used_mb, "cpu": cpu_usage_percent})
 
